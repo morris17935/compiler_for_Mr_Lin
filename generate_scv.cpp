a969da639d5f10cb1bf4aa6e21f5d77161223e97
morris <morris12345678987654321@gmail.com>
2017-09-25 17:35:07 +0800

    change

--- generate_scv.cpp
+++ generate_scv.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
-#include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string>
@@ -36,11 +35,11 @@ vector<moduel>XNOR;
 int startposition_x = 0;
 int startposition_y = 0;
 int turn = 0;
-void settinganswer(vector<moduel>*ptr){
+void settinganswer(vector<moduel> &caculate){
     int counting = 0;
     int countingforx = 25;
     int countingfory = 25;
-    for(moduel temp : *ptr){
+    for(moduel temp : caculate){
         bool input1ready = false;
         bool input2ready = false;
         if(temp.alreadypush == false){
@@ -48,13 +47,13 @@ void settinganswer(vector<moduel>*ptr){
                 for(moduel tempformoduel : tempforvectormoduel){
                     if(tempformoduel.output == temp.input1){
                         input1ready = true;
-                        *ptr[counting].firstinputx = countingforx;
-                        *ptr[counting].firstinputy = countingfory;
+						caculate[counting].firstinputx = countingforx;
+						caculate[counting].firstinputy = countingfory;
                     }
                     if(tempformoduel.output == temp.input2){
                         input2ready = true;
-                        *ptr[counting].secondinputx = countingforx;
-                        *ptr[counting].secondinputy = countingfory;
+						caculate[counting].secondinputx = countingforx;
+						caculate[counting].secondinputy = countingfory;
                     }
                     countingfory = countingfory + 100;
                 }
@@ -64,7 +63,7 @@ void settinganswer(vector<moduel>*ptr){
             if(input1ready == true && input2ready == true){
                 turn--;
                 tempforadded.push_back(temp);
-                *ptr[counting].alreadypush == true;
+				caculate[counting].alreadypush == true;
             }
         }
         counting++;
@@ -74,40 +73,49 @@ int main()
 {
 	char original[500] ;
 	vector<string> source;
-	if(stdin){
+	/*if(stdin){
 		while(fgets( original , 500, stdin ) != NULL){
 			string temp(original);
+			cout<<temp<<endl;
 			source.push_back(temp);
 		}
 		fclose(stdin);
 	}
 	else{
 		cout << "fail" ;
-	}
+	}*/
 	fstream file;
-    test.SVGBegin("answerforsvg",50000,50000);
+	file.open("design_00 .txt",ios::in);
+	do{
+        file.getline(original,sizeof(original));
+        string temp(original);
+        source.push_back(temp);
+	}while(!file.eof());
+	file.close();
+	test.SVGBegin("answerforsvg",50000,50000);
 	for(string temp : source){
-        if(temp.find("INPUT")>-1){
+        if(int(temp.find("INPUT("))>-1){
+            cout<<temp<<endl;
             moduel tempformoduel;
-            istringstream iss(temp.substr(temp.find('(') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss(temp.substr(temp.find('(') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             tempformoduel.name = "INPUT";
             tempformoduel.alreadypush = true;
             INPUT.push_back(tempformoduel);
         }
-        else if(temp.find("OUTPUT")>-1){
+        else if(int(temp.find("OUTPUT("))>-1){
             moduel tempformoduel;
-            istringstream iss(temp.substr(temp.find('(') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss(temp.substr(temp.find('(') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             tempformoduel.name = "OUTPUT";
             tempformoduel.alreadypush = true;
             INPUT.push_back(tempformoduel);
         }
-        else if(temp.find("AND")>-1){
+        else if(int(temp.find("AND("))>-1){
             moduel tempformoduel;
             istringstream iss(temp.substr(0,temp.find(" ")));
-            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1));
-            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1)));
+            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             iss2>>tempformoduel.input1;
             iss3>>tempformoduel.input2;
@@ -115,11 +123,11 @@ int main()
             tempformoduel.alreadypush = false;
             AND.push_back(tempformoduel);
         }
-        else if(temp.find("OR")>-1){
+        else if(int(temp.find("OR("))>-1){
             moduel tempformoduel;
             istringstream iss(temp.substr(0,temp.find(" ")));
-            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1));
-            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1)));
+            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             iss2>>tempformoduel.input1;
             iss3>>tempformoduel.input2;
@@ -127,20 +135,21 @@ int main()
             tempformoduel.alreadypush = false;
             OR.push_back(tempformoduel);
         }
-        else if(temp.find("NOT")>-1){
-            istringstream iss(0,temp.find(" ")));
-            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(')')-(temp.find('(') + 1));
+        else if(int(temp.find("NOT("))>-1){
+			moduel tempformoduel;
+			istringstream iss(temp.substr(0, temp.find(" ")));
+            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             iss2>>tempformoduel.input1;
             tempformoduel.name = "NOT";
             tempformoduel.alreadypush = false;
             NOT.push_back(tempformoduel);
         }
-        else if(temp.find("NAND")>-1){
+        else if(int(temp.find("NAND("))>-1){
             moduel tempformoduel;
             istringstream iss(temp.substr(0,temp.find(" ")));
-            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1));
-            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1)));
+            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             iss2>>tempformoduel.input1;
             iss3>>tempformoduel.input2;
@@ -148,11 +157,11 @@ int main()
             tempformoduel.alreadypush = false;
             NAND.push_back(tempformoduel);
         }
-        else if(temp.find("NOR")>-1){
+        else if(int(temp.find("NOR("))>-1){
             moduel tempformoduel;
             istringstream iss(temp.substr(0,temp.find(" ")));
-            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1));
-            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1)));
+            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             iss2>>tempformoduel.input1;
             iss3>>tempformoduel.input2;
@@ -160,11 +169,11 @@ int main()
             tempformoduel.alreadypush = false;
             NOR.push_back(tempformoduel);
         }
-        else if(temp.find("XOR")>-1){
+        else if(int(temp.find("XOR("))>-1){
             moduel tempformoduel;
             istringstream iss(temp.substr(0,temp.find(" ")));
-            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1));
-            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1)));
+            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             iss2>>tempformoduel.input1;
             iss3>>tempformoduel.input2;
@@ -172,11 +181,11 @@ int main()
             tempformoduel.alreadypush = false;
             XOR.push_back(tempformoduel);
         }
-        else if(temp.find("XNOR")>-1){
+        else if(int(temp.find("XNOR("))>-1){
             moduel tempformoduel;
-            istringstream iss(0,temp.find(" ")));
-            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1));
-            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1));
+            istringstream iss(temp.substr(0,temp.find(" ")));
+            istringstream iss2(temp.substr(temp.find('(') + 1,temp.find(',')-(temp.find('(') + 1)));
+            istringstream iss3(temp.substr(temp.find(',') + 1,temp.find(')')-(temp.find('(') + 1)));
             iss>>tempformoduel.output;
             iss2>>tempformoduel.input1;
             iss3>>tempformoduel.input2;
@@ -186,6 +195,7 @@ int main()
         }
     }
     turn = AND.size() + OR.size() + NOT.size() + NAND.size() + NOR.size() + XOR.size() + XNOR.size();
+    cout<<INPUT.size()<<endl;
     answer.push_back(INPUT);
     int coordinate = 0;
     for(moduel tempformoduel : answer[0]){
@@ -196,14 +206,14 @@ int main()
     while(turn != 0){
         startposition_x = startposition_x + 200;
         coordinate = 0;
-        settinganswer(&AND);
-        settinganswer(&OR);
-        settinganswer(&NOT);
-        settinganswer(&NAND);
-        settinganswer(&NOR);
-        settinganswer(&XOR);
-        settinganswer(&XNOR);
-        settinganswer(&OUTPUT);
+        settinganswer(AND);
+        settinganswer(OR);
+        settinganswer(NOT);
+        settinganswer(NAND);
+        settinganswer(NOR);
+        settinganswer(XOR);
+        settinganswer(XNOR);
+        settinganswer(OUTPUT);
         for(moduel tempformoduel : tempforadded){
             if(tempformoduel.name == "AND"){
                 test.drawAND(startposition_x,startposition_y + coordinate);
@@ -214,7 +224,7 @@ int main()
                 test.drawText(startposition_x,startposition_y + coordinate,tempformoduel.name);
             }
             else if(tempformoduel.name == "NOT"){
-                test.drawNOT(startposition_x,startposition_y + coordinate);
+                test.drawINV(startposition_x,startposition_y + coordinate);
                 test.drawText(startposition_x,startposition_y + coordinate,tempformoduel.name);
             }
             else if(tempformoduel.name == "NAND"){
@@ -261,7 +271,7 @@ int main()
                 test.drawPath(forpath2);
                 currenty = currenty + 100;
             }
-        currentx = currentx + 200
+			currentx = currentx + 200;
     }
     test.SVGEnd();
 }
